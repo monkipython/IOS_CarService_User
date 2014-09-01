de838fbae4e832137dad7fdb23729e4844ff1945
hgs泽泓 <hgszehong@hgsdream.local>
2014-09-01 15:24:52 +0800

    Initial Commit

---
+++ JiaKe/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  JiaKe
+//
+//  Created by hgs泽泓 on 14-9-1.
+//  Copyright (c) 2014年 HgsZehong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ JiaKeTests/JiaKeTests.m
@@ -0,0 +1,34 @@
+//
+//  JiaKeTests.m
+//  JiaKeTests
+//
+//  Created by hgs泽泓 on 14-9-1.
+//  Copyright (c) 2014年 HgsZehong. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface JiaKeTests : XCTestCase
+
+@end
+
+@implementation JiaKeTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
